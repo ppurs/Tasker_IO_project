b4dd9dfdb57b50949fe72dadb3c7e8b520eae47b
ppurs <paulinaaa523@gmail.com>
2022-05-28 20:59:20 +0200

    ZDN-10 feat: delete card

--- Project_Tasker/app/src/main/java/com/example/project_tasker/TasksActivity.java
+++ Project_Tasker/app/src/main/java/com/example/project_tasker/TasksActivity.java
@@ -1,11 +1,13 @@
 package com.example.project_tasker;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,6 +20,12 @@ public class TasksActivity extends AppCompatActivity {
 
     private static RecyclerView recViewTasks;
     private FloatingActionButton fabAddTask;
+    private int parentProjectIndex;
+    private Project parentProject;
+    private int parentCategoryIndex;
+    private Category parentCategory;
+    private int parentCardIndex;
+    private Card parentCard;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,14 +44,14 @@ public class TasksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
 
-        int parentProjectIndex = (int) getIntent().getExtras().get("parentProjectIndex");
-        Project parentProject = MainActivity.app.projects.get(parentProjectIndex);
+        parentProjectIndex = (int) getIntent().getExtras().get("parentProjectIndex");
+        parentProject = MainActivity.app.projects.get(parentProjectIndex);
 
-        int parentCategoryIndex = (int) getIntent().getExtras().get("parentCategoryIndex");
-        Category parentCategory = MainActivity.app.projects.get(parentProjectIndex).categories.get(parentCategoryIndex);
+        parentCategoryIndex = (int) getIntent().getExtras().get("parentCategoryIndex");
+        parentCategory = MainActivity.app.projects.get(parentProjectIndex).categories.get(parentCategoryIndex);
 
-        int parentCardIndex = (int) getIntent().getExtras().get("parentCardIndex");
-        Card parentCard = MainActivity.app.projects.get(parentProjectIndex).categories.get(parentCategoryIndex).cards.get(parentCardIndex);
+        parentCardIndex = (int) getIntent().getExtras().get("parentCardIndex");
+        parentCard = MainActivity.app.projects.get(parentProjectIndex).categories.get(parentCategoryIndex).cards.get(parentCardIndex);
 
         this.setTitle( parentProject.getName() + ", " + parentCategory.getName() + ", " + parentCard.getName());
 
@@ -73,10 +81,33 @@ public class TasksActivity extends AppCompatActivity {
         return recViewTasks;
     }
 
+    public void showAlertDialogDelete(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete card");
+        builder.setMessage("Are you sure you want to delete this card?");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                parentCategory.deleteCard( parentCardIndex );
+                CardsActivity.getRecViewCards().getAdapter().notifyDataSetChanged();
+
+                finish();
+
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item ) {
         switch (item.getItemId()) {
             case R.id.ic_delete:
+                showAlertDialogDelete( this.findViewById(android.R.id.content) );
                 return true;
             case R.id.ic_info:
                 return true;
