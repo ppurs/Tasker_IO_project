7dec0523ad1e98ebbd8add440fca822fdc063537
ppurs <paulinaaa523@gmail.com>
2022-05-29 21:16:43 +0200

    feat: category details + change dialog background

--- Project_Tasker/app/src/main/java/com/example/project_tasker/CardsActivity.java
+++ Project_Tasker/app/src/main/java/com/example/project_tasker/CardsActivity.java
@@ -6,12 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -23,6 +31,20 @@ public class CardsActivity extends AppCompatActivity {
     private Project parentProject;
     private int parentCategoryIndex;
     private Category parentCategory;
+    private TextView textName;
+    private TextView textDescription;
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        this.setTitle( "Category: " + parentCategory.getName() );
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable( parentCategory.getColor() ));
+
+        if ( textName != null && textDescription != null ) {
+            textName.setText( parentCategory.getName() );
+            textDescription.setText( parentCategory.getDescription() );
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,7 +68,8 @@ public class CardsActivity extends AppCompatActivity {
 
         parentCategoryIndex = (int) getIntent().getExtras().get("parentCategoryIndex");
         parentCategory = MainActivity.app.projects.get(parentProjectIndex).categories.get(parentCategoryIndex);
-        this.setTitle( "Project: " + parentProject.getName() + ", Category: " + parentCategory.getName());
+        this.setTitle( "Category: " + parentCategory.getName());
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable( parentCategory.getColor() ));
 
         recViewCards = findViewById(R.id.recViewCards);
 
@@ -95,6 +118,29 @@ public class CardsActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    public void showAlertDialogDetails(View view ) {
+
+        final Dialog dialog = new Dialog( CardsActivity.this );
+        dialog.setContentView(R.layout.dialog_details);
+        dialog.getWindow().setLayout( CardsActivity.this.getWindow().peekDecorView().getWidth(), ViewGroup.LayoutParams.WRAP_CONTENT );
+
+        textName = (TextView) dialog.findViewById(R.id.txtTitleName);
+        textName.setText( parentCategory.getName() );
+        textDescription = (TextView) dialog.findViewById(R.id.txtDescription );
+        textDescription.setText( parentCategory.getDescription() );
+
+        ImageButton editButton = (ImageButton) dialog.findViewById(R.id.imageButton );
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //costam
+            }
+        });
+
+        dialog.show();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item ) {
         switch (item.getItemId()) {
@@ -102,7 +148,7 @@ public class CardsActivity extends AppCompatActivity {
                 showAlertDialogDelete( this.findViewById(android.R.id.content) );
                 return true;
             case R.id.ic_info:
-                return true;
+                showAlertDialogDetails( this.findViewById(android.R.id.content) );
             default:
                 return super.onOptionsItemSelected(item);
         }
